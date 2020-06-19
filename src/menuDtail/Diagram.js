15d39e099769fa7001f9369af463ee4d2bd2b363
KingSE <susanso@naver.vom>
2020-06-19 18:33:53 +0900

    에니메이션 입히기 해치웠나?

--- src/menuDtail/Diagram.js
+++ src/menuDtail/Diagram.js
@@ -1,43 +1,128 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import {XYPlot, ArcSeries} from 'react-vis';
 import './MenuDtail.css'
+import Grid from '@material-ui/core/Grid';
+import { Hidden } from '@material-ui/core';
+import { Loop } from '@material-ui/icons';
 const Diagram=()=>{
 
-    
-    
-    const totalCount =58; //총 문제 갯수
-    const good = 57 ;// 맞은갯수
-    const notGood = totalCount-good;
-    const totalGoodPersant = good/totalCount*100; //정답률
-    const totalNotGoodPersant =(totalCount-good)/totalCount*100;  //오답률
+    const PI = Math.PI;
 
-    console.log(totalGoodPersant.toFixed(0),totalNotGoodPersant.toFixed(0))
-    const mygoodData = [
-        {angle0:totalCount/10, angle: notGood/10, radius: 60, radius0:80} // 정답률 출력
-      ]
-    const mynotGoodData = [
-        {angle0:totalCount/10, angle: good/10, radius: 40, radius0:60} // 오답률 출력
-    ]
+    const [totalCount,setTotalCount] =useState(58); //총 문제 갯수 
+    const [rightCount, setRightCount] = useState(20); // 맞은갯수
+    const [rightCountScore, setRightCountScore] = useState(rightCount/totalCount); //정답률
+    const [wrongCountScore, setWrongCountScore] = useState(1-rightCountScore)  //오답률
+    const [loopCnt, setLoopCnt] = useState(parseInt(rightCountScore*100));
+    const [myGoodData, setMyGoodData] = useState([{
+        angle0:0,
+        angle: 0,
+        radius: 50,
+        radius0:80
+    }]);
+    const [max, setMax] = useState(0);
+    const btnClick = () => {
+            console.log(max);
+            let data ={
+                angle0:0,
+                angle: -(2*PI*(max/100)),
+                radius: 50,
+                radius0:80
+            }
+            setMyGoodData([{...data}]);
+            
+        
+    }
+    useEffect(()=>{
+        // setInterval(()=>{
+        //     setMax(i);
+        //     document.getElementById('btn').click();
+        //     if(i==loopCnt){
+        //         return false;
+        //     }
+        // 
+        // var inter = setInterval(()=>{
+        //     setMax(max+1);
+        //     document.getElementById('btn').click();
+        //     console.log(max);
+        // },1000)
+        // btnClick();
+    },[]);
+    
+// 웹
+    // const myGoodData = [
+    //     {angle0:0, angle: -(2*PI*wrongCountScore) , radius: 50, radius0:80} //오답률 출력
+    //   ]
+    // const mynotGoodData = [
+    //     {angle0: 0 , angle: 2*PI*rightCountScore ,  radius: 50, radius0: 80}// 정답률 출력
+    // ]
+// 모바일
+    // const mygoodDataM = [
+    //     {angle0:0, angle: -(2*PI*wrongCountScore) , radius: 30, radius0:40} //오답률 출력
+    //   ]
+    // const mynotGoodDataM = [
+    //     {angle0: 0 , angle: 2*PI*rightCountScore ,  radius: 30, radius0: 40}// 정답률 출력
+    // ]
+    
     return(
         <div>
-                <XYPlot
-                    xDomain={[-5, 5]}
-                    yDomain={[-5, 5]}
-                    width={200}
-                    height={200}>
-                    <ArcSeries
-                        animation
-                        radiusType={'literal'}
-                        center={{x: -1, y: 0}}
-                        data={mygoodData}
-                        colorType={'literal'}/>
-                    <ArcSeries
-                        animation
-                        radiusType={'literal'}
-                        center={{x: -1, y: 0}}
-                        data={mynotGoodData}
-                        colorType={'literal'}/>
-                </XYPlot>
+            <button type="button" id="btn" onClick={btnClick}>버튼</button>
+            <Grid container>
+                 {/* 웹용 */}
+                 <Hidden only={['xs','sm']}>
+                    <Grid xs={12} md={12} lg={12}>
+                        <XYPlot
+                            xDomain={[-5, 5]}
+                            yDomain={[-5, 5]}
+                            width={200}
+                            height={200}>
+                            <ArcSeries
+                                animation
+                                radiusDomain={[0, 4]}
+                                radiusType={'literal'}
+                                center={{x: -1, y: 0}}
+                                data={myGoodData}
+                                color={'#f55e5e'}
+                                colorType={'literal'}/>
+                            {/* <ArcSeries
+                                animation
+                                radiusDomain={[0, 4]}
+                                radiusType={'literal'}
+                                center={{x: -1, y: 0}}
+                                data={mynotGoodData}
+                                color={'#289ee2'}
+                                colorType={'literal'}/> */}
+                        </XYPlot>
+                    </Grid>
+                </Hidden>
+                 {/* 모바일용 */}
+                 {/* <Hidden only={['md','lg','xl']}>
+                    <Grid xs={12} md={12}>
+                        <XYPlot
+                            xDomain={[-5, 5]}
+                            yDomain={[-5, 5]}
+                            width={100}
+                            height={100}>
+                            <ArcSeries
+                                animation
+                                radiusDomain={[0, 4]}
+                                radiusType={'literal'}
+                                center={{x: -3, y: -2}}
+                                data={mygoodDataM}
+                                color={'#f55e5e'}
+                                colorType={'literal'}/>
+                            <ArcSeries
+                                animation
+                                radiusDomain={[0, 4]}
+                                radiusType={'literal'}
+                                center={{x: -3, y: -2}}
+                                data={mynotGoodDataM}
+                                color={'#289ee2'}
+                                colorType={'literal'}/>
+                        </XYPlot>
+                    </Grid>
+                </Hidden> */}
+            </Grid>
+            
         </div>
     )
 }
