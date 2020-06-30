105c0b023ad8cba027f522ec244adcf262707cc9
Unknown <zkxm_0@naver.com>
2020-06-30 15:04:35 +0900

    폴더 늦는거 수정 완료

--- src/menuDtail/FolderAddList.js
+++ src/menuDtail/FolderAddList.js
@@ -1,4 +1,6 @@
 import React,{useState,useEffect} from "react";
+import { Paper } from '@material-ui/core';
+import StorefrontIcon from '@material-ui/icons/Storefront';
 
 const FolderAddList = (props) => {
     const [card,setCard] = useState(props.card);
@@ -16,46 +18,31 @@ const FolderAddList = (props) => {
             }
         })
 
-        setList([...data]);
         let data2=[];
-        study.map((studyItem,i) => {
-            data.map((listItem,idx) => {
-                if(studyItem.category === listItem.category && studyItem.cardset_no === listItem.cardset_no){
-                    data2.push({...studyItem,checked : 'true'});
-                }
-            })
-        })
-
-        setChList([...data2]);
-        let data3=[];
         study.map((studyItem,i) => {
             let re = data.map((listItem,idx) => {
                 if(studyItem.category === listItem.category && studyItem.cardset_no === listItem.cardset_no){
                     return false;
                 }else{
-                    return true;
+                    return listItem;
                 }
             })
-
             if(re.indexOf(false) === -1){
-                data3.push({...studyItem,checked : ''});
+                data2.push({...studyItem,checked : ''});
+            }else{
+                data2.push({...studyItem,checked : 'true'});
             }
         })
-        
-        let lastData = []
-        data2.map((item) => {
-            lastData.push({...item});
-        })
 
-        data3.map((item) => {
-            lastData.push({...item});
-        })
-
-        setList([...lastData]);
+        setChList([...data2]);
     }
     useEffect(() => {
         listDiv();
     },[])
+
+    useEffect(() => {
+        setList([...chList]);
+    },[chList])
 
     const checkboxClick = (e) => {
         let no = e.target.value;
@@ -77,65 +64,20 @@ const FolderAddList = (props) => {
                 list.map((item)=>{
                     return (
                         <>
-                            <div>
-                                <input type="checkbox" name="stlist" value={item.no} checked={item.checked} onClick={checkboxClick}/>{item.title}(카드수 : {item.cnt})<br/>
-                                <img src={item.profile} alt="" style={{width:'40px', height:'40px'}}/>{item.name}
-                            </div>
+                            <Paper className="fdr_on_add_fdr_content_box">
+                                <div className="fdr_on_add_modal_box">
+                                    <input type="checkbox" name="stlist" value={item.no} checked={item.checked} onChange={checkboxClick}/><span className="fdr_add_modal_con_sub_font">{item.title}()</span>
+                                    <img src={item.profile} alt=""/><span className="fdr_add_modal_con_name_font">{item.name}</span>
+                                    {/* 아이콘 */}
+                                    {
+                                        item.category === 'board' ? <StorefrontIcon/> : ''
+                                    }
+                                </div>
+                            </Paper>
                         </>
                     )
                 })  
             }
-            {/* {
-                list.map((item)=>{
-                    return (
-                        <>
-                            <div>
-                                <input type="checkbox" name="stlist" value={item.no} checked={item.checked}/>{item.title}({item.category})<br/>
-                                <img src={item.profile} alt="" style={{width:'40px', height:'40px'}}/>{item.name}
-                            </div>
-                        </>
-                    )
-                }) 
-            } */}
-            {/* {
-                study.map((studyItem,idx) => {
-                    return(
-                        <>
-                            {
-                               re = card.map((cardItem,i) => {
-                                    temp = studyItem;
-                                    if(cardItem.folder_no === parseInt(no)){
-                                        if(cardItem.cardset_no === studyItem.cardset_no && cardItem.category === studyItem.category){
-                                            return(
-                                                <>
-                                                
-                                                    <div>
-                                                        <input type="checkbox" name="stlist" value={studyItem.no} checked/>{studyItem.title}({studyItem.category})<br/>
-                                                        <img src={studyItem.profile} alt="" style={{width:'40px', height:'40px'}}/>{studyItem.name}
-                                                    </div>
-                                                </>
-                                            )
-                                        }else{
-                                            return false
-                                        }
-                                        // if(i === card.length-1){
-                                        //     return(
-                                        //         <>
-                                                
-                                        //             <div>
-                                        //                 <input type="checkbox" name="stlist" value={temp.no}/>{temp.title}({temp.category})<br/>
-                                        //                 <img src={temp.profile} alt="" style={{width:'40px', height:'40px'}}/>{temp.name}
-                                        //             </div>
-                                        //         </>
-                                        //     )
-                                        // }
-                                    }
-                                })
-                            }
-                        </>
-                    )               
-                })
-            } */}
         </>
     );
 }
--- src/menuDtail/FolderDetail.js
+++ src/menuDtail/FolderDetail.js
@@ -7,14 +7,7 @@ const FolderDetail=(props)=>{
     let card = props.card;
     let folder_no = props.no;
     let study = props.study;
-    const check = () => {
-        card.map((item, idx) => {
-            if(item.folder_no === parseInt(folder_no)){
-                console.log(item);
-            }
-        })
-    }
-    
+        
     return(
         <>
             {
@@ -25,7 +18,6 @@ const FolderDetail=(props)=>{
                                 {
                                     study.map((studyItem,i) => {
                                         if(studyItem.cardset_no === item.cardset_no && studyItem.category === item.category){
-                                            console.log(studyItem);
                                             return(
                                                 <>
                                                     <div className="fdr_set_box" style={{ boxShadow: '0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)'}}>
